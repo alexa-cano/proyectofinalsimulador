133b498fbef67eb369b84112262351f987f105fc
alexacanoayon@gmail.com <alexacanoayon@gmail.com>
2023-08-11 22:16:17 -0600

    se agregó localstorge y estilos css

--- main.js
+++ main.js
@@ -1,6 +1,5 @@
 // Esta función calcula el resultado de la calculadora
 function calcular() {
-
   // Obtenemos los elementos del formulario que contienen los datos del usuario
   let agua = document.getElementById("water-input").value; // Litros de agua al día
   let energia = document.getElementById("energy-input").value; // kWh de energía eléctrica al mes
@@ -36,62 +35,83 @@ function calcular() {
   // Se calcula el impacto ambiental del consumo de agua
   resultado += agua * factorAgua;
 
-// Se calcula el impacto ambiental del consumo de energía
-resultado += energia * factorEnergia;
+  // Se calcula el impacto ambiental del consumo de energía
+  resultado += energia * factorEnergia;
 
-// Se calcula el impacto ambiental del transporte
-resultado += transporte * distancia * factoresTransporte[transporte];
+  // Se calcula el impacto ambiental del transporte
+  resultado += transporte * distancia * factoresTransporte[transporte];
 
-// Se calcula el impacto ambiental de los residuos
-if (organico) {
-resultado += residuo * factoresResiduo[0];
-}
-if (papel) {
-resultado += residuo * factoresResiduo[1];
-}
-if (plastico) {
-resultado += residuo * factoresResiduo[2];
-}
-if (metal) {
-resultado += residuo * factoresResiduo[3];
-}
-if (vidrio) {
-resultado += residuo * factoresResiduo[4];
+  // Se calcula el impacto ambiental de los residuos
+  if (organico) {
+    resultado += residuo * factoresResiduo[0];
+  }
+  if (papel) {
+    resultado += residuo * factoresResiduo[1];
+  }
+  if (plastico) {
+    resultado += residuo * factoresResiduo[2];
+  }
+  if (metal) {
+    resultado += residuo * factoresResiduo[3];
+  }
+  if (vidrio) {
+    resultado += residuo * factoresResiduo[4];
+  }
+
+  // Se redondea el resultado a dos decimales
+  resultado = resultado.toFixed(2);
+
+  // Se genera el mensaje según el resultado obtenido
+  if (resultado < 0) {
+    mensaje = "¡Felicidades! Tu huella ecológica es negativa, lo que significa que estás contribuyendo a reducir las emisiones de gases de efecto invernadero.";
+  } else if (resultado == 0) {
+    mensaje = "Tu huella ecológica es nula, lo que significa que no estás generando ni reduciendo las emisiones de gases de efecto invernadero.";
+  } else {
+    mensaje = "Tu huella ecológica es de " + resultado + " kg CO2e al día, lo que significa que estás generando una cantidad de gases de efecto invernadero que afectan al cambio climático. Te recomendamos que adoptes hábitos más sostenibles para reducir tu impacto ambiental.";
+  }
+
+   // Se muestra el resultado y el mensaje en el div correspondiente
+   const resultDiv = document.getElementById("result");
+   resultDiv.innerHTML = "Resultado: " + resultado + " kg CO2e al día. <br>" + mensaje;
+
+  // Guardar resultados en localStorage
+  localStorage.setItem("resultado", resultado);
+  localStorage.setItem("mensaje", mensaje);
 }
 
-// Se redondea el resultado a dos decimales
-resultado = resultado.toFixed(2);
+// Esta función muestra el resultado almacenado en localStorage
+function mostrarResultado() {
+  const resultadoGuardado = localStorage.getItem("resultado");
+  const mensajeGuardado = localStorage.getItem("mensaje");
 
-// Se genera el mensaje según el resultado obtenido
-if (resultado < 0) {
-mensaje = "¡Felicidades! Tu huella ecológica es negativa, lo que significa que estás contribuyendo a reducir las emisiones de gases de efecto invernadero.";
-} else if (resultado == 0) {
-mensaje = "Tu huella ecológica es nula, lo que significa que no estás generando ni reduciendo las emisiones de gases de efecto invernadero.";
-} else {
-mensaje = "Tu huella ecológica es de " + resultado + " kg CO2e al día, lo que significa que estás generando una cantidad de gases de efecto invernadero que afectan al cambio climático. Te recomendamos que adoptes hábitos más sostenibles para reducir tu impacto ambiental.";
-}
-
-// Se muestra el resultado y el mensaje en el div correspondiente
-document.getElementById("result").innerHTML = "Resultado: " + resultado + " kg CO2e al día. <br>" + mensaje;
+  if (resultadoGuardado !== null && mensajeGuardado !== null) {
+    const resultDiv = document.getElementById("result");
+    resultDiv.innerHTML = "Resultado: " + resultadoGuardado + " kg CO2e al día. <br>" + mensajeGuardado;
+  }
 }
 
 // Esta es la función que se ejecuta cuando la página se carga
 function iniciar() {
-// Se asignan valores aleatorios a los elementos del formulario para simular un usuario
-document.getElementById("water-input").value = Math.floor(Math.random() * 100);
-document.getElementById("energy-input").value = Math.floor(Math.random() * 500);
-document.getElementById("transport-input").value = Math.floor(Math.random() * 5);
-document.getElementById("distance-input").value = Math.floor(Math.random() * 50);
-document.getElementById("organic-input").checked = Math.random() < 0.5;
-document.getElementById("paper-input").checked = Math.random() < 0.5;
-document.getElementById("plastic-input").checked = Math.random() < 0.5;
-document.getElementById("metal-input").checked = Math.random() < 0.5;
-document.getElementById("glass-input").checked = Math.random() < 0.5;
-document.getElementById("waste-input").value = Math.floor(Math.random() * 10);
+  // Se asignan valores aleatorios a los elementos del formulario para simular un usuario
+  document.getElementById("water-input").value = Math.floor(Math.random() * 100);
+  document.getElementById("energy-input").value = Math.floor(Math.random() * 500);
+  document.getElementById("transport-input").value = Math.floor(Math.random() * 5);
+  document.getElementById("distance-input").value = Math.floor(Math.random() * 50);
+  document.getElementById("organic-input").checked = Math.random() < 0.5;
+  document.getElementById("paper-input").checked = Math.random() < 0.5;
+  document.getElementById("plastic-input").checked = Math.random() < 0.5;
+  document.getElementById("metal-input").checked = Math.random() < 0.5;
+  document.getElementById("glass-input").checked = Math.random() < 0.5;
+  document.getElementById("waste-input").value = Math.floor(Math.random() * 10);
 
-// Se llama a la función calcular() para obtener el resultado inicial
-calcular();
+  // Llama a la función calcular() para obtener el resultado inicial
+  calcular();
+
+  // Mostrar resultado si ya se ha calculado previamente
+  mostrarResultado();
 }
+ // Mostrar el resultado
+ document.getElementById("calculator-form").classList.add("show-result");
 
 // Se llama a la función iniciar() cuando la página se carga
 window.onload = iniciar;
